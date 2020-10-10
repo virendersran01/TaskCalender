f537e852ba82b43930361b8370488cea05c50e1f
virendersran01 <virendersran01@gmail.com>
2020-10-10 13:42:13 +0530

    working on login screen.

--- app/src/main/java/com/virtualstudios/taskcalender/activities/MainActivity.java
+++ app/src/main/java/com/virtualstudios/taskcalender/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.virtualstudios.taskcalender;
+package com.virtualstudios.taskcalender.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.virtualstudios.taskcalender.R;
 
 public class MainActivity extends AppCompatActivity {
 
