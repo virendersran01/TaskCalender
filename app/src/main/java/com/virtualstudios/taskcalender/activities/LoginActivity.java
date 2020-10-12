d41806059459f59ea2e998e999076215e25bb12d
virendersran01 <virendersran01@gmail.com>
2020-10-12 16:36:19 +0530

    working on bottom app bar.

--- app/src/main/java/com/virtualstudios/taskcalender/activities/LoginActivity.java
+++ app/src/main/java/com/virtualstudios/taskcalender/activities/LoginActivity.java
@@ -2,15 +2,25 @@ package com.virtualstudios.taskcalender.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.virtualstudios.taskcalender.R;
+import com.virtualstudios.taskcalender.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonGoogle.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainActivity.class));
+        });
     }
 }
